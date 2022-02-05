75ae48685f438c54b249924bb72d87027e7a4f47
Sanusi Abdulkadir <94573138+sanusisusi@users.noreply.github.com>
2022-02-06 00:06:45 +0100

    Add files via upload

---
+++ src/component/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
+import "./App.css";
+
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <header>REACT CALCULATOR</header>
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+        <footer>&copy; 2022 Sanusi Abdulkadir<br/>Software Engineer (React developer)</footer>
+      </div>
+    );
+  }
+}
